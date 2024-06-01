0adb4b36ccc1f635d144064ed4bd2b3c93ca5079
Bibek773 <157711841+Bibek773@users.noreply.github.com>
2024-06-01 13:56:32 +0545

    fuck

---
+++ Parameterized_constructor.c
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class hariom {
+private:
+    int area;
+
+public:
+    hariom() {
+       area= l*b;
+       cout<<
+    }
+
+    ~hariom() {
+
+    }
+};
+
+int main() {
+    hariom bibek(4,5);
+    return 0;
+}
